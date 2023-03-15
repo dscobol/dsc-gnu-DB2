b113fe40bbe230443a2f0144accec0b34549a876
dscobol <dscobol@gmail.com>
2023-03-14 22:11:29 -0400

    Convert GETDBID to ACCEPT from ENVIRONMENT.

--- Basic-Sample/jcl/dbcbex13-run.sh
+++ Basic-Sample/jcl/dbcbex13-run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Set Up
+# Set Up variables
+# Program to run
 PGM=DBCBEX13
+
+# Export the environment variables in the .env file
+export $(grep -v '^#' ../.env | xargs)
+
 # Setup Report
 export DD_OUTFILE="../spool/dbcbex13-report.rpt"
 
 # run program
 ../bin/$PGM
+
+# Remove the variables in the .env file from the environment
+unset $(grep -v '^#' ../.env | sed -E 's/(.*)=.*/\1/' | xargs)
